5aeb876f1744d9ab0e67126844eb3b4eb5cc211f
abhishekkeshri <abhishek.keshri@dunzo.in>
2023-02-01 01:29:00 +0530

    Store get list of product service added

--- models/StoreModel.go
+++ models/StoreModel.go
@@ -39,10 +39,33 @@ func (s *StoreModel) GetProductsInStore(db *gorm.DB, limit, start int) []Product
 	return products
 }
 
-// todo AddProducts
-func (s *StoreModel) AddProducts() bool {
+func (s *StoreModel) AddProducts(db *gorm.DB, products []ProductModel) bool {
 
-	return false
+	tx := db.Begin()
+	for i := 0; i < len(products); i++ {
+		res := db.Create(&products[i])
+		fmt.Println(products[i].ID, products[i].Name)
+		if res.Error != nil {
+			tx.Rollback()
+			break
+		}
+
+		s.ProductId = int64(products[i].ID)
+		s.IsAvailable = true
+		result := db.Model(&s).Where("store_id = ? and product_id = ?", s.StoreId, s.ProductId).Updates(&s)
+		if result.RowsAffected == 0 {
+			result = db.Create(&s)
+		}
+		if result.Error != nil {
+			tx.Rollback()
+			break
+		}
+	}
+	if tx.Error != nil {
+		return false
+	}
+	err := tx.Commit().Error
+	return err == nil
 }
 
 func (s *StoreModel) TableName() string {
--- services/Stores.go
+++ services/Stores.go
@@ -1,5 +1,46 @@
 package services
 
+import (
+	"github.com/abhikeshri07/go-mux/models"
+	"github.com/abhikeshri07/go-mux/utils"
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+	"net/http"
+	"strconv"
+)
+
 type Stores struct {
 	products IProducts
+	conn     *gorm.DB
+}
+
+type IStores interface {
+	GetProducts(w http.ResponseWriter, r *http.Request)
+}
+
+func (s *Stores) GetProducts(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	limit, _ := strconv.Atoi(r.FormValue("limit"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid Store ID")
+		return
+	}
+	store := models.StoreModel{StoreId: int64(id)}
+	products := store.GetProductsInStore(s.conn, limit, start)
+
+	if products == nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "Some Error Occurred")
+		return
+	}
+	utils.RespondWithJSON(w, http.StatusOK, products)
+}
+
+// AddProducts todo
+func (s *Stores) AddProducts(w http.ResponseWriter, r *http.Request) {
+
+}
+func NewStore(conn *gorm.DB) *Stores {
+	return &Stores{conn: conn}
 }
